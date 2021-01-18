141ba426f3bad65dc8656037543258ad23880672
Erik Szabó <erik1szabo1@gmail.com>
2021-01-18 22:08:30 +0100

    small redesign to post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,5 @@
-import { useRouter } from "next/router";
 import Head from "next/head";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import matter from "gray-matter";
 import styles from "../../styles/Post.module.css";
@@ -10,40 +9,22 @@ import { getNoteMarkdown } from "../../lib/requests";
 const thumbnailBaseUrl =
   "https://raw.githubusercontent.com/ErikSzabo/bprof-notes-data/master/assets/thumbnails/";
 
-const Post = ({ markdown }) => {
-  const [content, setContent] = useState("");
-  const [meta, setMeta] = useState({});
-  const router = useRouter();
-  const { id } = router.query;
-
-  useEffect(() => {
-    // setMeta(matter(content).data);
-    if (!id) return;
-    getNoteMarkdown(id).then((res) => {
-      setContent(res);
-      setMeta(matter(res).data);
-    });
-  }, [id]);
+const Post = ({ markdown, metaData }) => {
+  const [content, setContent] = useState(markdown);
+  const [meta, setMeta] = useState(JSON.parse(metaData));
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>{`Jegyzet - ${id && id.slice(11)}`}</title>
+        <title>{`Jegyzet - ${meta.title}`}</title>
       </Head>
 
-      <div className={styles.mdContainer}>
-        <div className={styles.metaContainer}>
-          <img
-            className={styles.smallPostThumbnail}
-            src={meta.thumbnail && `${thumbnailBaseUrl}${meta.thumbnail}`}
-          />
-          <div className={styles.meta}>
-            <h1 className="title">{meta.title}</h1>
-            <h3>{meta.description}</h3>
-            <h6>{meta.date && meta.date.toLocaleDateString()}</h6>
-          </div>
-        </div>
+      <div className={styles.metaContainer}>
+        <p>{meta.title}</p>
+        <p>{new Date(meta.date).toLocaleDateString()}</p>
+      </div>
 
+      <div className={styles.mdContainer}>
         <ReactMarkdown>{content}</ReactMarkdown>
       </div>
       <Footer />
@@ -51,24 +32,18 @@ const Post = ({ markdown }) => {
   );
 };
 
-// export const getStaticPaths = async () => {
-//   const blogs = await getAllNotesMetadata();
-//   const mdFileNames = blogs.map((blog) => blog.fileName);
-//   const paths = mdFileNames.map((fileName) => ({ params: { fileName } }));
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+  const markdown = await getNoteMarkdown(id);
+  const metaData = JSON.stringify(matter(markdown).data);
+  console.log(metaData);
 
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
-
-// export const getStaticProps = async ({ params: { fileName } }) => {
-//   const markdown = await getNoteMarkdown(fileName);
-//   return {
-//     props: {
-//       markdown,
-//     },
-//   };
-// };
+  return {
+    props: {
+      markdown,
+      metaData,
+    },
+  };
+}
 
 export default Post;
