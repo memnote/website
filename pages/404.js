74e3f3da44aed320c7ac767055889ee19ebe26a8
Erik Szabó <erik1szabo1@gmail.com>
2021-01-25 19:53:08 +0100

    added title to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import Head from "next/head";
 import styles from "../styles/404.module.css";
 
 export default function FourOhFour() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Memnote - Az oldal nem található</title>
+      </Head>
+
       <h1>Memnote</h1>
       <h2>404 - Az oldal nem található</h2>
       <Link href="/">Vissza a főoldalra</Link>
