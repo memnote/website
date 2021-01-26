9964f87b1b37ef40bf38d9a4a64645d7b7ec4971
Erik Szabó <erik1szabo1@gmail.com>
2021-01-26 13:21:00 +0100

    removed mocked api data

--- pages/api/meta.js
+++ pages/api/meta.js
@@ -28,41 +28,32 @@ export const getMetaData = async (
 ) => {
   let response = [];
 
-  for (let i = 0; i < 100; i++) {
-    response.push({
-      title: `Teszt cím ${i}`,
-      subject: "szoftech",
-      description: "Teszt leírás.",
-      date: "2020-01-12",
+  if (cache.hasCache()) {
+    response = cache.getAll();
+  } else {
+    const rawMeta = await axios.get(`${baseURL}/metadata/`, {
+      headers: {
+        Authorization: `token ${process.env.TOKEN}`,
+      },
     });
+
+    const metaDataPromises = rawMeta.data.map(
+      (data) =>
+        new Promise(async (resolve, reject) => {
+          const json = await axios.get(`${scrapeURL}/metadata/${data.name}`);
+          const parsedJSON = json.data;
+          resolve({
+            ...parsedJSON,
+            fileName: data.name.replace("-metadata.json", ""),
+          });
+        })
+    );
+
+    response = await Promise.all(metaDataPromises);
+    cache.save(response);
   }
 
-  // if (cache.hasCache()) {
-  //   response = cache.getAll();
-  // } else {
-  //   const rawMeta = await axios.get(`${baseURL}/metadata/`, {
-  //     headers: {
-  //       Authorization: `token ${process.env.TOKEN}`,
-  //     },
-  //   });
-
-  //   const metaDataPromises = rawMeta.data.map(
-  //     (data) =>
-  //       new Promise(async (resolve, reject) => {
-  //         const json = await axios.get(`${scrapeURL}/metadata/${data.name}`);
-  //         const parsedJSON = json.data;
-  //         resolve({
-  //           ...parsedJSON,
-  //           fileName: data.name.replace("-metadata.json", ""),
-  //         });
-  //       })
-  //   );
-
-  //   response = await Promise.all(metaDataPromises);
-  //   cache.save(response);
-  // }
-
-  // response.sort(sortDescendent);
+  response.sort(sortDescendent);
 
   if (subject) response = response.filter((post) => post.subject === subject);
   if (search)
