ad7d7c13ed4bdac5e02d04d1eb1d3c6ffba2fa63
Erik Szabó <erik1szabo1@gmail.com>
2021-02-04 17:06:34 +0100

    makeStyles not working quick fix

--- components/ScrollTop.js
+++ components/ScrollTop.js
@@ -1,28 +1,8 @@
 import React, { useState, useEffect, useCallback } from "react";
 import ArrowUp from "@material-ui/icons/KeyboardArrowUp";
 import IconButton from "@material-ui/core/IconButton";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles(() => ({
-  button: {
-    position: "fixed",
-    bottom: "30px",
-    right: "30px",
-    transition: "all 0.3s",
-    userSelect: "none",
-    WebkitTouchCallout: "none",
-  },
-  arrow: {
-    width: "50px",
-    height: "50px",
-    userSelect: "none",
-    color: "black",
-    WebkitTouchCallout: "none",
-  },
-}));
 
 function ScrollTop() {
-  const classes = useStyles();
   const [visible, setVisible] = useState(false);
 
   const scrollTop = () => {
@@ -41,14 +21,27 @@ function ScrollTop() {
   return (
     <>
       <IconButton
-        className={classes.button}
         style={{
           opacity: visible ? "1" : "0",
           cursor: visible ? "pointer" : "auto",
+          position: "fixed",
+          bottom: "30px",
+          right: "30px",
+          transition: "all 0.3s",
+          userSelect: "none",
+          WebkitTouchCallout: "none",
         }}
         onClick={scrollTop}
       >
-        <ArrowUp className={classes.arrow} />
+        <ArrowUp
+          style={{
+            width: "50px",
+            height: "50px",
+            userSelect: "none",
+            color: "black",
+            WebkitTouchCallout: "none",
+          }}
+        />
       </IconButton>
     </>
   );
